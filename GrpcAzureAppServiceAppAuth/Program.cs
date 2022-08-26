3f13d3b6b27d2a1650cb32609f1abb42ab2a3d28
damienbod <damien_bod@hotmail.com>
2022-08-26 07:49:12 +0200

    Init greater service

--- GrpcAzureAppServiceAppAuth/Program.cs
+++ GrpcAzureAppServiceAppAuth/Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Authentication;
+using GrpcAzureAppServiceAppAuth;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Identity.Web;
 
@@ -8,25 +8,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddGrpc();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllers();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGrpcService<GreeterService>();
+});
 
 app.Run();
