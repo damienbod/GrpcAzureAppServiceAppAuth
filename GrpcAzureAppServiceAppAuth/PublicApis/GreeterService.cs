ea0aa541e4666a4c686437757f0e3a9a7af36a1a
damienbod <damien_bod@hotmail.com>
2024-06-22 06:23:22 +0200

    small code clean up

--- GrpcAzureAppServiceAppAuth/PublicApis/GreeterService.cs
+++ GrpcAzureAppServiceAppAuth/PublicApis/GreeterService.cs
@@ -11,7 +11,7 @@ public class GreeterService : Greeter.GreeterBase
     {
         return Task.FromResult(new HelloReply
         {
-            Message = "Hello " + request.Name
+            Message = $"Hello {request.Name}"
         });
     }
 }
